c490f35721ea952ce85c683f78a6f9e4bb71e88b
f1205718939 <3387044889@qq.com>
2022-07-26 00:06:04 +0800

    修改启动类

--- new1/src/main/java/com/xx/NewApplication.java
+++ new1/src/main/java/com/xx/NewApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class NewApplication {
 
     public static void main(String[] args) {
+        int i =1;
         SpringApplication.run(NewApplication.class, args);
     }
 
